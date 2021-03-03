9cdb81a95f464aa29db9cbe4bf1a1eed845e704f
brunopenso <viperp@gmail.com>
2021-03-02 22:10:38 -0300

    add body parser

--- src/express/index.js
+++ src/express/index.js
@@ -1,11 +1,14 @@
 'use strict'
 const express = require('express')
+const bodyParser = require('body-parser')
 const routes = require('../business/routes')
 const { runtime, validateRoute } = require('multi-integration-library')
 
 validateRoute(routes)
 
 const app = express()
+app.use(bodyParser.json())
+
 app.all('/*', function (req, res) {
   runtime(routes, req, res)
 })
